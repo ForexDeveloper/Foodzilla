ec7ed5f5766b1a8e40e6b65998c92aedfe12cc2a
JavidPour.Mohammad <JavidPour.Mohammad@TEK001N4446>
2023-12-04 21:21:43 +0330

    Push

--- Foodzilla.Persistence.EF/Configurations/ChiefOfficers/ChiefExecutiveOfficerConfiguration.cs
+++ Foodzilla.Persistence.EF/Configurations/ChiefOfficers/ChiefExecutiveOfficerConfiguration.cs
@@ -1,5 +1,6 @@
-﻿using Foodzilla.Domain.Aggregates.ChiefOfficers;
+﻿using Microsoft.EntityFrameworkCore;
 using Foodzilla.Kernel.Persistence.EF.Commons;
+using Foodzilla.Domain.Aggregates.ChiefOfficers;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Foodzilla.Persistence.EF.Configurations.ChiefOfficers;
@@ -9,5 +10,20 @@ public sealed class ChiefExecutiveOfficerConfiguration : IdentityConfiguration<C
     public override void Configure(EntityTypeBuilder<ChiefExecutiveOfficer> builder)
     {
         builder.Property(p => p.AssignedOrganization).HasMaxLength(ColumnLength.Length250).IsRequired();
+
+        builder.HasOne(p => p.ChiefMarketingOfficer)
+               .WithOne(p => p.ChiefExecutiveOfficer)
+               .HasForeignKey(nameof(ChiefProductOfficer.ChiefExecutiveOfficerId))
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(p => p.ChiefProductOfficer)
+               .WithOne(p => p.ChiefExecutiveOfficer)
+               .HasForeignKey(nameof(ChiefProductOfficer.ChiefExecutiveOfficerId))
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(p => p.ChiefTechnicalOfficer)
+               .WithOne(p => p.ChiefExecutiveOfficer)
+               .HasForeignKey(nameof(ChiefTechnicalOfficer.ChiefExecutiveOfficerId))
+               .OnDelete(DeleteBehavior.Restrict);
     }
 }
